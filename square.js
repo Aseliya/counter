f03d781302f9bab3dce00d5400acbae11bbabac1
Асель Кенжебекова <aselkenzebekova@MacBook-Air-Asel.local>
2019-07-29 09:30:08 +0600

    initial commit

---
+++ square.js
@@ -0,0 +1,16 @@
+let count=0
+let square = document.getElementById("square")
+square.onclick=function(){
+  count = count + 1
+  x = Math.floor(Math.random() * 256);
+  y = Math.floor(Math.random() * 256);
+  z = Math.floor(Math.random() * 256);
+  let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+  square.style.background = bgColor
+  this.innerHTML=count
+let table = document.getElementById("statistic");
+let tr = document.createElement("tr");
+tr.innerHTML = `<td>${count}</td> <td>${bgColor}</td>`
+table.appendChild(tr);
+}
